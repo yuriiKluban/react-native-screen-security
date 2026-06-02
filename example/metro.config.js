5208f190a4f04ba68f576abeb0ac727068792cfc
Yurii Kluban <ykluban9@gmail.com>
2026-06-02 20:00:44 +0300

    chore: update package.json and metro.config.js for improved module resolution and remove unused dependencies

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,8 +1,46 @@
+const fs = require('fs');
 const path = require('path');
-const { getDefaultConfig } = require('@react-native/metro-config');
-const { withMetroConfig } = require('react-native-monorepo-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
-module.exports = withMetroConfig(getDefaultConfig(__dirname), {
-  root: path.resolve(__dirname, '..'),
-  dirname: __dirname,
-});
+const root = path.resolve(__dirname, '..');
+const exampleModules = path.resolve(__dirname, 'node_modules');
+const rootModules = path.resolve(root, 'node_modules');
+
+function resolvePackage(name) {
+  const inExample = path.join(exampleModules, name);
+  if (fs.existsSync(inExample)) {
+    return inExample;
+  }
+  return path.join(rootModules, name);
+}
+
+const reactDir = resolvePackage('react');
+const reactNativeDir = resolvePackage('react-native');
+const hasExampleReactNative = fs.existsSync(path.join(exampleModules, 'react-native'));
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+const config = {
+  watchFolders: [root],
+  resolver: {
+    nodeModulesPaths: [exampleModules, rootModules],
+    extraNodeModules: {
+      react: reactDir,
+      'react-native': reactNativeDir,
+    },
+    ...(hasExampleReactNative
+      ? {
+          blockList: [
+            new RegExp(`${escapePath(rootModules)}/react-native/.*`),
+            new RegExp(`${escapePath(rootModules)}/react/.*`),
+          ],
+        }
+      : {}),
+  },
+};
+
+function escapePath(p) {
+  return p.replace(/[/\\]/g, '[/\\\\]');
+}
+
+module.exports = mergeConfig(defaultConfig, config);
