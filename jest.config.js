1dfde98fbcfee9e141a15b0fe07d49a26fe3a209
Yurii Kluban <ykluban9@gmail.com>
2026-06-04 12:04:40 +0300

    fix: align react-test-renderer with react 19.2.3 for CI tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   preset: '@react-native/jest-preset',
+  moduleNameMapper: {
+    '^react$': require.resolve('react'),
+    '^react-test-renderer$': require.resolve('react-test-renderer'),
+  },
   modulePathIgnorePatterns: ['<rootDir>/lib/', '<rootDir>/example/'],
   testPathIgnorePatterns: ['<rootDir>/example/'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
